fd1686dd9393f7a44eb4ef51b4939a91cefc8291
tuchan2002 <trananhtu171002@gmail.com>
2024-12-13 13:25:40 +0700

    setup vueuse/motion

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -47,113 +47,134 @@
       posuere. Pellentesque dignissim scelerisque eros in placerat. Sed non
       ipsum enim.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul data-aos="fade-up">
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div
+      v-motion
+      :initial="{
+        opacity: 0,
+        y: 100,
+      }"
+      :visible-once="{
+        opacity: 1,
+        y: 0,
+      }"
+      :delay="200"
+      :duration="1200"
+    >
+      <h3>Installed CLI Plugins</h3>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+            target="_blank"
+            rel="noopener"
+            >babel</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
+            target="_blank"
+            rel="noopener"
+            >pwa</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
+            target="_blank"
+            rel="noopener"
+            >router</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
+            target="_blank"
+            rel="noopener"
+            >vuex</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+            target="_blank"
+            rel="noopener"
+            >eslint</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
+            target="_blank"
+            rel="noopener"
+            >typescript</a
+          >
+        </li>
+      </ul>
+      <h3>Essential Links</h3>
+      <ul>
+        <li>
+          <a href="https://vuejs.org" target="_blank" rel="noopener"
+            >Core Docs</a
+          >
+        </li>
+        <li>
+          <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+            >Forum</a
+          >
+        </li>
+        <li>
+          <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+            >Community Chat</a
+          >
+        </li>
+        <li>
+          <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+            >Twitter</a
+          >
+        </li>
+        <li>
+          <a href="https://news.vuejs.org" target="_blank" rel="noopener"
+            >News</a
+          >
+        </li>
+      </ul>
+      <h3>Ecosystem</h3>
+      <ul>
+        <li>
+          <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+            >vue-router</a
+          >
+        </li>
+        <li>
+          <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
+            >vuex</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-devtools#vue-devtools"
+            target="_blank"
+            rel="noopener"
+            >vue-devtools</a
+          >
+        </li>
+        <li>
+          <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+            >vue-loader</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/awesome-vue"
+            target="_blank"
+            rel="noopener"
+            >awesome-vue</a
+          >
+        </li>
+      </ul>
+    </div>
+    <img src="https://picsum.photos/200/300" />
   </div>
 </template>
 
@@ -170,11 +191,14 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.hello {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
 }
 p {
-  margin: 50px 250px;
+  max-width: 360px;
 }
 ul {
   list-style-type: none;
